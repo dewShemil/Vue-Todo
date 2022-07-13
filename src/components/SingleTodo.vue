056b756daacb4afe6c6c6ef54814e6ff444e839d
batsdk <shemilkaweesha@gmail.com>
2022-07-13 17:37:51 +0530

    Components Created

---
+++ src/components/SingleTodo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Single Todo</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
